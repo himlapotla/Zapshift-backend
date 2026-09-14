2a359792e514743774b520d449bccb9ca23bb2eb
potla <shahriarhimel00@gmail.com>
2026-09-14 16:27:03 +0600

    site domain aded part 3

--- index.js
+++ index.js
@@ -559,8 +559,6 @@ app.post('/create-checkout-session', async (req, res) => {
 
     success_url: `${process.env.SITE_DOMAIN_APP}/dashboard/payment-successs?session_id={CHECKOUT_SESSION_ID}`,
     
-    
-
     // CHECKOUT_SESSION_ID -- Stripe replaces this automatically with the actual Checkout Session ID after successful payment or creating a session.
     cancel_url: `${process.env.SITE_DOMAIN_APP}/dashboard/payment-canceled`,
 
